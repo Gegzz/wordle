1199652e3b7e1886487432771e13132a2f583647
Gegzz <ggegeshidze95@gmail.com>
2022-06-04 18:50:17 +0400

    gege

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import Board from "./Components/Board";
 import Keyboard from "./Components/Keyboard";
-import { boardDefault } from "./Words";
+import { boardDefault, generateWordSet } from "./Words";
 import { createContext } from "react";
+import { useEffect } from "react";
 
 export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const correctWord = "GREED";
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+    });
+  }, []);
 
   const onSelectLetter = (keyVal) => {
-    debugger;
     if (currAttempt.letterPos > 4) return;
     const newBoard = [...board];
     newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
@@ -33,7 +41,20 @@ function App() {
 
   const onEnter = () => {
     if (currAttempt.letterPos !== 5) return;
-    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+
+    let currWord = "";
+
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      alert("Word not found");
+    }
+    if (currWord === correctWord) {
+      alert("Game Ended");
+    }
   };
   return (
     <div className="App">
@@ -49,6 +70,9 @@ function App() {
           onSelectLetter,
           onEnter,
           onDelete,
+          correctWord,
+          setDisabledLetters,
+          disabledLetters,
         }}
       >
         <div className="game">
--- src/Components/Key.js
+++ src/Components/Key.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
 
-function Key({ keyVal, bigKey }) {
+function Key({ keyVal, bigKey, disabled }) {
   const { onSelectLetter, onEnter, onDelete } = useContext(AppContext);
   const selectLetter = () => {
     if (keyVal === "Enter") {
@@ -14,7 +14,7 @@ function Key({ keyVal, bigKey }) {
     }
   };
   return (
-    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+    <div className="key" id={bigKey ? "big": disabled && 'disabled'} onClick={selectLetter}>
       {keyVal}
     </div>
   );
